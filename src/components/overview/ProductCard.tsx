d808895bc18a3b4b72826888fb1b3b24ca31c984
ABDhaikal <kakikuatku.gmail.com>
2025-03-10 11:41:47 +0700

    fix:card

--- src/components/overview/ProductCard.tsx
+++ src/components/overview/ProductCard.tsx
@@ -11,6 +11,7 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({data}) => {
    return (
       <div className="bg-[#543a2d] w-[20rem] rounded-2xl overflow-hidden ">
+         <div className="text-[20px] text-[#f8e4be] md:text-2xl text-center bg-[#463325] p-4"> {data.name}</div>
          <Link href ={`/product/${data.slug}`} className="aspect-square relative block ">
             <Image src={data.image} fill alt="product" />
          </Link>
